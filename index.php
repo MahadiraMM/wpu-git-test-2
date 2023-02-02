c0c966d4ab36c9136f426ef0f196065cd9f0ad34
Mahadira <mahadiramahadira@gmail.com>
2023-02-02 15:49:07 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quibusdam. Vero repudiandae, nisi earum necessitatibus dignissimos magnam animi quos recusandae nemo at quia adipisci autem architecto quam dicta neque? Ea?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 
 </body>
 </html>
